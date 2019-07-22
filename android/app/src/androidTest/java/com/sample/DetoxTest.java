6e6761847e5bb6a99d2ef732d748c33ce6584029
Mason Wesolek <masonwesolek15@gmail.com>
2019-07-22 14:02:57 -0500

    .

--- android/app/src/androidTest/java/com/sample/DetoxTest.java
+++ android/app/src/androidTest/java/com/sample/DetoxTest.java
@@ -1,7 +1,7 @@
 package com.sample;
 
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
+import androidx.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.sampleprojectreactnative.MainActivity;
